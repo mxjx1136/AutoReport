9422c6740683466af2323c2c0ad974a31fae8aa9
mxjx1136 <79369182+mxjx1136@users.noreply.github.com>
2022-08-03 18:07:35 +0800

    Update yqtb.py

--- yqtb.py
+++ yqtb.py
@@ -10,7 +10,7 @@ headers = {
 }
 
 root_url = 'http://pubinfo.sdwcvc.cn'
-login_url = http://pubinfo.sdwcvc.cn/index.jsp'
+login_url ='http://pubinfo.sdwcvc.cn/index.jsp'
 
 def login(username, password):
     login_data = json.dumps( {'user_account': username, 'user_password': password})
